943c1f4cd3359065a8620faf0eedb399466817b9
Marketing Director <nicolestark1103@gmail.com>
2023-10-31 15:16:52 -0500

    added schemas folder and documents and started reconfiguring server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { typeDefs, resolvers } = require('./schemas')
 
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +22,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once('open', async () => {
+  
+  await apolloServer.start()
+  
+  app.use('/graphql', expressMiddleware(apolloServer, {
+    context: async ({ req }) => ({ token: req.headers.token })
+  }));
+
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on http://localhost:${PORT}`)
+    console.log(`GraphQL available at http://localhost:${PORT}/graphql`)
+  });
 });
