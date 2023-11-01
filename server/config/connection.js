1f8f1f2ae3bf2d810d24a758059a74aad1784d17
Marketing Director <nicolestark1103@gmail.com>
2023-11-01 14:44:48 -0500

    added some to config not sure if needed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindandModify: false
+    }
+);
 
 module.exports = mongoose.connection;
